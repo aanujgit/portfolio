4014dd8e3d6403c4ee81642267f4da43f1089fba
Anuj Singh <anujsingh454568@gmail.com>
2026-05-12 20:59:33 +0530

    my portfolio website

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Portfolio Loaded");
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+  sections.forEach((section) => {
+
+    const sectionTop = section.offsetTop - 300;
+
+    if(window.scrollY >= sectionTop){
+      section.style.opacity = "1";
+      section.style.transform = "translateY(0px)";
+    }
+
+  });
+
+});
